0334c1448a44cddafff933bb8666b9178e9f2293
zanzat123 <khairulizzat@graduate.utm.my>
2022-12-18 21:32:42 +0800

    main

---
+++ src/test/java/project/PCMS/PcmsApplicationTests.java
@@ -0,0 +1,13 @@
+package project.PCMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
