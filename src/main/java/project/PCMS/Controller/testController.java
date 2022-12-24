95afb796db280b32978ae3e0c8b4091c2f1cd533
zanzat123 <khairulizzat@graduate.utm.my>
2022-12-24 16:55:11 +0800

    izzat/connect login into index

--- src/main/java/project/PCMS/Controller/testController.java
+++ src/main/java/project/PCMS/Controller/testController.java
@@ -2,15 +2,22 @@ package project.PCMS.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class testController {
-
-    @GetMapping("/index")
-    public String index() {
-        return "index";
+    
+    //startup auto login page
+    @GetMapping("/")
+    public String loginstart() {
+        return "login";
     }
     
+    @PostMapping("/index")
+    public String index(){
+        return "index";
+    }
+
 }
