e2122136910b3084b23c34a70b178a4cb67872d9
faruqazln <ahmadfaruq@graduate.utm.my>
2023-01-27 22:48:53 +0800

    faruq/model-doctor-patient-admin

---
+++ src/main/java/project/PCMS/Model/Admin.java
@@ -0,0 +1,72 @@
+package project.PCMS.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Admin {
+
+    // id will be auto generate
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    // define inside the column
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "fullname")
+    private String fullname;
+
+    public  Admin (String username, String password, String fullname) {
+    this.username = username;
+    this.password = password;
+    this.fullname = fullname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getfullname() {
+        return fullname;
+    }
+
+    public void setfullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getusername() {
+        return username;
+    }
+
+    public void setusername(String username) {
+        this.username = username;
+    }
+
+    public String getpassword() {
+        return password;
+    }
+
+    public void setpassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "admin [ fullname=" + fullname + ", password=" + password + "]";
+    }
+}
+
