c35c592276eebcfbb314d24f68f0fc1987ad012a
zanzat123 <khairulizzat@graduate.utm.my>
2023-01-31 00:26:07 +0800

    createDoctorController/ijat

---
+++ src/main/java/project/PCMS/Controller/DoctorController.java
@@ -0,0 +1,30 @@
+package project.PCMS.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import project.PCMS.Model.CounsellingSession;
+import project.PCMS.Repository.BookCounsellingSessionRepository;
+
+@Controller
+public class DoctorController {
+
+    @Autowired
+    BookCounsellingSessionRepository cousellingrepo;
+
+    @GetMapping("/doctor/")
+    public String assigncounselling(@RequestParam("counsellingsessionId")Long counsellingsessionId,
+                                    @RequestParam("doctorid")Long doctorId, Model model){
+
+        
+        CounsellingSession CS = cousellingrepo.getReferenceById(counsellingsessionId);
+        System.out.println(CS);
+
+
+        return "doctordashboard";
+    }
+    
+}
